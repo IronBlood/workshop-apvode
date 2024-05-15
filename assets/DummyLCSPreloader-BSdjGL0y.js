b84172937a6ad32a66cf1bb7efdd5fff70439432
IronBlood <IronBlood@users.noreply.github.com>
2024-05-15 09:26:14 +0000

    deploy: 40466d64443ef2d79bf1e249b4c1f9519346cfb0

--- assets/DummyLCSPreloader-BSdjGL0y.js
+++
@@ -1,1 +0,0 @@
-import{L as e}from"./Utils-BbtiyQYk.js";import{d as o,G as r}from"./vendor-70biTZpe.js";const _=o({__name:"DummyLCSPreloader",setup(a){return r(()=>{e()}),(n,t)=>null}});export{_ as default};
--- assets/main-D5lLZQFH.js
+++
@@ -1,8 +0,0 @@
-const n=`#include <stdio.h>
-
-int main() {
-	printf("hello world\\n");
-	return 0;
-}
-
-`;export{n as C};
